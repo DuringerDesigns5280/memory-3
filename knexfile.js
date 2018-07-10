d8cb496748ce2c24d596b36c4528c50280f07f09
Taylor <tayjed2010@gmail.com>
2018-07-10 14:41:07 -0600

    fixed production

--- knexfile.js
+++ knexfile.js
@@ -4,12 +4,11 @@ module.exports = {
     client: 'pg',
     connection: 'postgres:///games',
   },
+
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+    connection: process.env.DATABASE_URL + '?ssl=true'
 
+  }
 };
+
